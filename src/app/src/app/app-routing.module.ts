a002d8a7d6cd9154a28050394bca9a11a506101d
Evangelia <evangelia.mitsopoulou@gmail.com>
2019-11-03 15:37:53 +0100

    statistics

--- src/app/src/app/app-routing.module.ts
+++ src/app/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ScenariodetailviewComponent } from './components/scenariodetailview/scenariodetailview.component';
 import { LearningGoalComponentComponent } from './components/learning-goal-component/learning-goal-component.component';
-
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 
 const routes: Routes = [
   {path: '', component: LearningGoalComponentComponent},
   { path: 'scenario-view', component: ScenariodetailviewComponent },
+  { path: 'statistics-view', component: StatisticsComponent },  
 ];
 
 @NgModule({
--- src/app/src/app/components/statistics/statistics.component.ts
+++ src/app/src/app/components/statistics/statistics.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Peter', weight: 7, symbol: '70%'},
+  {position: 2, name: 'Maria', weight: 6, symbol: '55%'},
+  {position: 3, name: 'Kate', weight: 3, symbol: '30%'},
+  {position: 4, name: 'John', weight: 2, symbol: '28%'},
+];
+
+
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
