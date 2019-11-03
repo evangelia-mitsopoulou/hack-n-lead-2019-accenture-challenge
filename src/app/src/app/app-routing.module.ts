c7314776fb81c3dd3093cdacbfbd2710a940f214
Evangelia <evangelia.mitsopoulou@gmail.com>
2019-11-03 10:26:51 +0100

    angular routing

--- src/app/src/app/app-routing.module.ts
+++ src/app/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ScenariodetailviewComponent } from './components/scenariodetailview/scenariodetailview.component';
+import { LearningGoalComponentComponent } from './components/learning-goal-component/learning-goal-component.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: LearningGoalComponentComponent},
+  { path: 'scenario-view', component: ScenariodetailviewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
